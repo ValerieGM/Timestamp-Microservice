313f5fa7111aac40b994a2e9f0a7ecc163e78ede
ValerieGM <vgongora@student.wethinkcode.co.za>
2021-06-20 22:06:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Timestamp-Microservice
+
+Timestap Microservice in NodeJS and Express.
+
+## Project Setup
+
+``` #! /bin /bash
+npm install
+```
+
+### Runs the app in developement mode
+
+``` #! /bin /bash
+npm start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+const parseTime = (req, ress, next) => {
+
+    next();
+}
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function(req, res) {
+    res.json({ greeting: 'hello API' });
+});
+
+// api/timestamp
+app.get("/api/timestamp", (req, res) => {
+    res.json(
+        req.time = {
+            unix: new Date().toString(),
+            utc: new Date().getTime()
+        }
+    );
+});
+
+// api/timestamp:date?
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
